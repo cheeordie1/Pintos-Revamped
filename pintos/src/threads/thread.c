67761d280d6d4b4540bd7b1c4434b905caf8fdf4
cheeordie1 <cheeawai@stanford.edu>
2015-07-20 23:34:21 -0700

    Part 2.1 working

--- pintos/src/threads/thread.c
+++ pintos/src/threads/thread.c
@@ -216,7 +216,7 @@ thread_cmp (const struct list_elem *a, const struct list_elem *b,
 {
   struct thread *thread_a = list_entry (a, struct thread, elem);
   struct thread *thread_b = list_entry (b, struct thread, elem);
-  return thread_a->priority < thread_b->priority;
+  return thread_a->priority > thread_b->priority;
 }
 
 /* Puts the current thread to sleep.  It will not be scheduled
