b403f59186651588504b5fe88dda30763342cc6a
cheeordie1 <cheeawai@stanford.edu>
2016-06-11 02:45:52 -0700

    migrating to local

--- pintos/src/threads/palloc.c
+++ pintos/src/threads/palloc.c
@@ -59,6 +59,9 @@ palloc_init (size_t user_page_limit)
   init_pool (&kernel_pool, free_start, kernel_pages, "kernel pool");
   init_pool (&user_pool, free_start + kernel_pages * PGSIZE,
              user_pages, "user pool");
+
+  /* TODO Initialize frame table for the user pool. The table contains
+     user_pages amount of frames. */
 }
 
 /* Obtains and returns a group of PAGE_CNT contiguous free pages.
